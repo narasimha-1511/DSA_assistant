09b51b0e5e55d7ff635e57c082808cc87dbbbb98
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-16 15:55:37 +0530

    basic frontend

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/navbar.css";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <h1 style={{ fontSize: "24px" }}> DSA Assistant</h1>
+    </div>
+  );
+};
+
+export default NavBar;
